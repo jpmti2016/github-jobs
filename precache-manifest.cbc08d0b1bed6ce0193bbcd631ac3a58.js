d93468b837a18810129758e7e166b1701a0b36e1
Yampier Medina <jpmti2016@gmail.com>
2020-09-20 12:51:45 -0400

    Updates

---
+++ precache-manifest.cbc08d0b1bed6ce0193bbcd631ac3a58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da14ad73ff0249b99c7891163d619377",
+    "url": "/github-jobs/index.html"
+  },
+  {
+    "revision": "fd62a10fa1d551da9d31",
+    "url": "/github-jobs/static/css/main.ea614c5b.chunk.css"
+  },
+  {
+    "revision": "e304cf6ab8df2af91d3c",
+    "url": "/github-jobs/static/js/2.26651bee.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/github-jobs/static/js/2.26651bee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd62a10fa1d551da9d31",
+    "url": "/github-jobs/static/js/main.92cc06f0.chunk.js"
+  },
+  {
+    "revision": "818ca7b73b62caac58fd",
+    "url": "/github-jobs/static/js/runtime-main.90d76a25.js"
+  },
+  {
+    "revision": "fc568113ad72433c2e3802588369dd2a",
+    "url": "/github-jobs/static/media/backgroundImg.fc568113.png"
+  }
+]);
